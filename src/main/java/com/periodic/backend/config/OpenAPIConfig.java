19e23a6ce380755a87936478a9df7079aa99e982
dinhhieu2003 <vuongdinhhieu2003@gmail.com>
2025-04-01 11:40:22 +0700

    MODIFY: rename server url for testing on swagger

--- src/main/java/com/periodic/backend/config/OpenAPIConfig.java
+++ src/main/java/com/periodic/backend/config/OpenAPIConfig.java
@@ -59,7 +59,7 @@ public class OpenAPIConfig {
                 .info(createApiInfo())
                 .servers(List.of(
                         createServer("http://localhost:8080", "Server URL in Development environment"),
-                        createServer("https://toeic-practice-hze3cbbff4ctd8ce.southeastasia-01.azurewebsites.net", "Server URL in Production environment")))
+                        createServer("https://periodic-table-b6dwgehjdvcteufk.southeastasia-01.azurewebsites.net", "Server URL in Production environment")))
                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
                 .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
     }
